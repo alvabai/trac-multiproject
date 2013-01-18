465b9ec91a80f55f746e33be8618c0d347881cc4
Martti Haukijärvi <martti.haukijarvi@eficode.com>
2013-01-18 16:17:39 +0200

    added tests

--- plugins/multiproject/multiproject/core/util/test_util.py
+++ plugins/multiproject/multiproject/core/util/test_util.py
@@ -11,6 +11,7 @@ import util
 from util import sanitize_html
 
 class Test_sanitize_html(unittest.TestCase):
+    """Test util.util.sanitize_html"""
 
     def setUp(self):
         pass
@@ -25,7 +26,28 @@ class Test_sanitize_html(unittest.TestCase):
         res = sanitize_html("")
         self.assertEqual(res, "")
 
+    def test_removing_tags(self):
+        """Don't trim spaces""" 
+        res = sanitize_html(' <foo>my text here!</foo>')
+        self.assertEqual(res, " ")
 
+    def test_removing_element(self):
+        """Remove html elements"""
+        html = "playnice<something>nasty<!-- javascript --></something>"
+        res = sanitize_html(html)
+        self.assertEqual(res, "playnice")
+
+    def test_remove_lone_tags(self):
+        """One-tag -elements should be removed."""
+        html = "<foo/>Leave me"""
+        res = sanitize_html(html)
+        self.assertEqual(res, "Leave me")
+
+    def test_remove_script(self):
+        """Remove javascript."""
+        html = """<p onClick="alert('Hello');">foo</p>"""
+        res = sanitize_html(html)
+        self.assertEqual(res, "<p>foo</p>")
 
 
 if __name__ == '__main__':
