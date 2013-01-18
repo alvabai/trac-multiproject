ba3eb5427a3baa8312b315535dbd44667e151caa
Martti Haukijärvi <martti.haukijarvi@eficode.com>
2013-01-18 14:17:56 +0200

    added testing with emty string

--- plugins/multiproject/multiproject/core/util/test_util.py
+++ plugins/multiproject/multiproject/core/util/test_util.py
@@ -3,17 +3,29 @@
 
 import unittest
 
+# TODO kludge: move path setting to a runner script
+import sys
+sys.path.append('../../..')
+
 import util
 from util import sanitize_html
 
-class test_sanitize_html(unittest.TestCase):
+class Test_sanitize_html(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_empty(self):
+    def test_None(self):
+        """With None return empty string"""
         res = sanitize_html(None)
         self.assertEqual(res, "")
+
+    def test_empty(self):
+        """Without input return empty string"""
+        res = sanitize_html("")
+        self.assertEqual(res, "")
+
+
 
 
 if __name__ == '__main__':
