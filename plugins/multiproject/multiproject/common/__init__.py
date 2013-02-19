44c297066a72bff2d552f7c6e01cedad02645bcc
Martti Haukijärvi <martti.haukijarvi@eficode.com>
2013-02-19 15:36:09 +0200

    changed absolute imports to relative

--- plugins/multiproject/multiproject/common/__init__.py
+++ plugins/multiproject/multiproject/common/__init__.py
@@ -1,18 +1,18 @@
 import env
 import help
-from multiproject.common import membership
-from multiproject.common import projects
-from multiproject.common import users
-from multiproject.common import web
-from multiproject.common import wiki
-from multiproject.common import analytics
-from multiproject.common import admin
-from multiproject.common import pagination
-from multiproject.common import messages
-from multiproject.common import notifications
-from multiproject.common import featured
-from multiproject.common.staticfiles import GlobalStaticRequestFiles
-from multiproject.common.hacks import *
-from multiproject.common.xmlrpc import *
-from multiproject.common.permissions import GlobalPermissionPolicy, GlobalPermissionStore
-from multiproject.common import siteadmin
+import membership
+import projects
+import users
+import web
+import wiki
+import analytics
+import admin
+import pagination
+import messages
+import notifications
+import featured
+from staticfiles import GlobalStaticRequestFiles
+from hacks import *
+from xmlrpc import *
+from permissions import GlobalPermissionPolicy, GlobalPermissionStore
+import siteadmin
