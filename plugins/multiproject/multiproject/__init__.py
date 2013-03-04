aa70d6ed8b17675dee5bb836f8e49ee781e9ebfc
Martti Haukijärvi <martti.haukijarvi@eficode.com>
2013-03-04 10:14:51 +0200

    Added docstring.

--- plugins/multiproject/multiproject/__init__.py
+++ plugins/multiproject/multiproject/__init__.py
@@ -1,3 +1,6 @@
+"""This is the main package of the project. 
+Subfolders map to features: see README.
+"""
 
 # Please do not import anything (heavy) here for performance reasons
 __version__ = '1.4.23'
