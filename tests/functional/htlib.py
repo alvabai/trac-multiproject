5388b0dd851c041995017fd03bef33a6ebc10786
Martti Haukijärvi <martti.haukijarvi@eficode.com>
2013-04-09 15:40:11 +0300

    Logging in and fetching /home works

--- tests/functional/htlib.py
+++ tests/functional/htlib.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-def get_session_cookie(cookies, key="trac"):
-    """Return a list of cookies and their values having key in name.
+def get_session_cookies(cookies, key="trac"):
+    """Return a string of cookies and their values having key in name.
 
-    >>> get_session_cookie(['foo_session=a0jkas0, expires=Thu, 04-Jul-2013 04:52:37 GMT; Path=/home', 'foo_form=4de338; bar=34asf;'], key="foo")
+    >>> get_session_cookies(['foo_session=a0jkas0, expires=Thu, 04-Jul-2013 04:52:37 GMT; Path=/home', 'foo_form=4de338; bar=34asf;'], key="foo")
     'foo_session=a0jkas0; foo_form=4de338'
 
-    >>> get_session_cookie(['foo_session=abc, expires=Thu, 04-Jul-2013 04:52:37 GMT; Path=/home'], key="foo")
+    >>> get_session_cookies(['foo_form_token=d984; Path=/home', 'foo_session=bb64312be8e55f0a10a49c10; expires=Thu, 04-Jul-2013 15:08:14 GMT; Path=/home'], key="form")
+    'foo_form_token=d984'
+
+    >>> get_session_cookies(['foo_session=abc, expires=Thu, 04-Jul-2013 04:52:37 GMT; Path=/home'], key="foo")
     'foo_session=abc'
 
     """
 
-    s_cookies = [parse_cookie(c, key) for c in cookies]
+    if not type(cookies) == list:
+        cookies = [cookies]
+    assert(type(cookies) == list),  "cookies is not a list"
+    s_cookies = [parse_cookie(c, key) for c in cookies if key in c]
     return "; ".join(s_cookies)
+    #return dict([k,v for x.split('=') in s_cookies])
+
+
+def get_session_dict(cookies, key="session"):
+    """Return a dict with cookies but strip all cookie attributes.
+
+    >>> get_session_dict('foo_session=890az; expires=Thu, 04-Jul-2013 04:52:37 GMT; Path=/home')
+    {'foo_session': '890az'}
+    """
+
+    ckd = dict()
+    ck_and_value, sep, tail = cookies.partition(';')
+    cookie, value = ck_and_value.split('=')
 
 
 def parse_cookie(cookie, sub="session"):
@@ -22,22 +41,27 @@ def parse_cookie(cookie, sub="session"):
     >>> parse_cookie('foo_session=890az; expires=Thu, 04-Jul-2013 04:52:37 GMT; Path=/home')
     'foo_session=890az'
 
-    >>> parse_cookie('foo_session=890az; foo_auth=4de33; expires=Sun, 07 Apr 2013 08:04:33; Path=/', sub="auth")
+    >>> parse_cookie('foo_session=890az; foo_auth=4de33; foo_form_token=1234; expires=Sun, 07 Apr 2013 08:04:33; Path=/', sub="auth")
     'foo_auth=4de33'
+
+    >>> parse_cookie('foo_session=890az; foo_auth=4de33; foo_form_token=1234; expires=Sun, 07 Apr 2013 08:04:33; Path=/', sub="form")
+    'foo_form_token=1234'
     """
 
+    assert(type(cookie) == str) , "cookie is not a string but a " % type (cookie)
     session_strings = [x.rstrip(' ,;') for x in cookie.split() if sub in x]
 
     # we don't expect more than one session value in one cookie
-    assert(len(session_strings) == 1) 
+    assert(len(session_strings) == 1), "more than one matching value"
     return session_strings.pop()
-
 
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+
 
 # vim: sw=4
 
--- tests/functional/httplibrarytest.robot
+++ tests/functional/httplibrarytest.robot
@@ -1,6 +1,7 @@
 *** Settings ***
 Resource       ${ENVIRONMENT}.txt
 Library        HttpLibrary.HTTP
+Library        htlib
 
 *** Variables ***
 ${proj_name}  test_project6
@@ -30,15 +31,27 @@ Follow redirect
 Login
   Create HTTP Context   localhost:4433  https
   GET  /
-  Follow Response  
-  Set Basic Auth  tracadmin   tracadmin
-  POST  /home/user?goto=https://localhost:4433/home/
-  Log Response Status  
-  Log Response Headers  
-  Response Status Code Should Equal  200
-  Set Request Body  username=tracadmin&password=tracadmin&action=do_login
-  POST  /home/user/
+  Follow Response
+  ${chead}=  Get Response Header  set-cookie  
+  ${c1}=  Get Session Cookies  ${chead}
+  Log  ${c1}
+  ${form_token}=  Get Session Cookies  ${chead}  key=form_token
+  ${form_token}=  Evaluate  str("${form_token}").split('=')[1]
+  Log  ${form_token}
+  Set Request Header  Cookie  ${c1}
+  Set Request Body  __FORM_TOKEN=${form_token}&username=tracadmin&password=tracadmin&login=Login&action=do_login
+  POST  /home/user
+  ${all_cookies}=  Get Response Header  set-cookie
+  ${auth_cookie}=  Get Session Cookies  ${all_cookies}  key=auth 
+  Log  ${auth_cookie}
+  Set Request Header  Cookie  ${c1}; ${auth_cookie}
+  GET  /home/
+  #${body}=  Get response body
+  #Log  ${body}
+  #Response Status Code Should Equal  200
+  #Set Request Body  username=tracadmin&password=tracadmin&action=do_login
   Show Response Body In Browser
-  Response Status Code Should Equal  200
+  #POST  /home/user/
+  #Response Status Code Should Equal  200
 
 *** Keywords ***
