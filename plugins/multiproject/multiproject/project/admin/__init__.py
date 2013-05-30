21c0165bddba269f0ad68561a52e297bd7069c69
Dexter Ogie-osahon <dexter@ogie-osahon.lan>
2013-05-30 10:35:43 +0300

    ticket#1792 Removes entire Admin#Storages section

--- plugins/multiproject/multiproject/project/admin/__init__.py
+++ plugins/multiproject/multiproject/project/admin/__init__.py
@@ -1,7 +1,6 @@
 from multiproject.project.admin.admintemplateprovider import *
 from multiproject.project.admin.basics import *
 from multiproject.project.admin.categories import *
-from multiproject.project.admin.storages import *
 from multiproject.project.admin.system import *
 from multiproject.project.admin.news_forum import *
 from multiproject.project.admin.vcm import *
